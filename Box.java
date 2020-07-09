273931ab8d33fb4a74d888dc773e6732a4da192a
Toni <68050663+unrealtoni@users.noreply.github.com>
2020-07-09 10:59:13 +0200

    Add files via upload

---
+++ Box.java
@@ -0,0 +1,54 @@
+package ExerciseEncapsulation;
+
+public class Box {
+    private double length;
+    private double width;
+    private double height;
+
+    public Box(double length, double width, double height){
+        this.setLength(length);
+        this.setWidth(width);
+        this.setHeight(height);
+    }
+
+    private void setLength(double length) {
+        if (length>=1) {
+            this.length = length;
+        }else {
+            throw new IllegalArgumentException("Length cannot be zero or negative.");
+        }
+    }
+
+    private void setWidth(double width) {
+        if (width >=1) {
+            this.width = width;
+        }else {
+            throw new IllegalArgumentException("Width cannot be zero or negative");
+        }
+    }
+
+    public void setHeight(double height) {
+        if (height>=1) {
+            this.height = height;
+        }else {
+            throw new IllegalArgumentException("Height cannot be zero or negative");
+        }
+    }
+
+    public double getSurfeaceArea(){
+        return 2* this.length* this.width + 2* this.length* this.height + 2* this.width* this.height;
+    }
+    public double getLateralSA(){
+        return 2* this.length* this.height + 2* this.width* this.height;
+    }
+    public double getVolume(){
+        return this.length* this.width* this.height;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Surface area - %.2f%n" +
+                "Lateral Surface Area - %.2f%n" +
+                "Volume - %.2f%n",getSurfeaceArea(),getLateralSA(),getVolume());
+    }
+}
---
+++ Main.java
@@ -0,0 +1,21 @@
+package ExerciseEncapsulation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        double length = Double.parseDouble(reader.readLine());
+        double width = Double.parseDouble(reader.readLine());
+        double height = Double.parseDouble(reader.readLine());
+        Box box;
+        try {
+            box = new Box(length, width, height);
+            System.out.println(box);
+        }catch (IllegalArgumentException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+}
